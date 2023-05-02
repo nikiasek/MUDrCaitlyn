0482171e8ae5f0b87b3f73511513bb08587f0994
nikiasek <nikiasek50@gmail.com>
2023-05-02 22:58:38 +0200

    recode/cleanup
    * Cleanup of useless files
    * Bit of recode, using ttkbootstrap

---
+++ recode/Scripts/Credentials/firstTime.py
@@ -0,0 +1,2 @@
+def everythingforfirsttime():
+    print("idiot")
---
+++ recode/main.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+from os import path
+from recode.Scripts.Credentials.firstTime.py import everythingforfirsttime as ft
+
+
+def credential():
+    if path.exists("recode\Scripts\Credentials\Credentials.json"):
+        ft()
+    else:
+        pass
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # Windows settings
+        self.geometry(f"{1280}x{720}")
+        self.title("Professor Caitlyn")
+        self.iconbitmap("Recode\Caitlyn.ico")
+
+
+
+if __name__ == "__main__":
+    credential()
+    window = App()
+    window.mainloop()
