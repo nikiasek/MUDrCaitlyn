34af461090b2bf401f9cc4ed966a09ba63994b8d
nikiasek <nikiasek50@gmail.com>
2023-05-05 18:03:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ def credential():
     else:
         pass
 
-class App(tk.Tk):
+class App(ttk.Window):
     def __init__(self):
         super().__init__()
+        ## == SETTINGS == ##
         # ttkbootstrap style
-
+        style = ttk.Style(theme="firsttime")
+        window = style.master
 
         # centering Window
         ws = self.winfo_screenwidth()
@@ -33,41 +35,48 @@ class App(tk.Tk):
         self.overrideredirect(True)
         self.configure(bg="#856ff8")
 
-        ## == ALL WIDGETS == ##
-        # background
-        bgcanvas =  tk.Canvas(self,
-                            width=400,
-                            height=400,
-                            bd=0,
-                            highlightthickness=0,)
-
-        bgcanvas.pack(fill = "both", expand=True)
-        
+        # Creating Containter
         background = PhotoImage(file =r"Assets\\background.png")
         self.background = background
-        
-        # background Image
-        bgcanvas.create_image((0, 0), image=background, anchor = "nw")
+        containter = ttk.Frame(self)
 
-        # Title text
-        miniHeader = "this is a sign-up"
-        mainHeader = "Welcome in PeriCait"
-        infoHeader1 = "When you first start this app, we need" 
-        infoHeader2 = "some info to display right informations!"
-
-        bgcanvas.create_text(200, 30, text=miniHeader, fill="#C8AA6E", font=("Beaufort for LOL", 14), width=380)
-        bgcanvas.create_text(200, 70, text=mainHeader, fill="#F0E6D2", font=("Beaufort for LOL", 28), width=380)
-        bgcanvas.create_text(200, 120, text=infoHeader1, fill="#A09B8C", font=("Spiegel Regular", 12), width=380)
-        bgcanvas.create_text(200, 140, text=infoHeader2, fill="#A09B8C", font=("Spiegel Regular", 12), width=380)
-        bgcanvas.pack()
-
-        # Region select
-        regions= ["EUNE", "EUW"]
-        currentVar = tk.StringVar()
-        regionSelector = ttk.Combobox(bgcanvas, width= 30, textvariable= currentVar, values=regions)
-        regionSelector.pack(pady=170)
-
-
+        ## == ALL WIDGETS == ##
+        # background
+        #bgcanvas =  ttk.Canvas(self,
+        #                    width=400,
+        #                    height=400,
+        #                    bd=0,
+        #                    highlightthickness=0,)
+#
+        #bgcanvas.pack(fill = "both", expand=True)
+        #
+        #background = PhotoImage(file =r"Assets\\background.png")
+        #self.background = background
+        #
+        ## background Image
+        #bgcanvas.create_image((0, 0), image=background, anchor = "nw")
+#
+        ## Title text
+        #miniHeader = "this is a sign-up"
+        #mainHeader = "Welcome in PeriCait"
+        #infoHeader1 = "When you first start this app, we need" 
+        #infoHeader2 = "some info to display right informations!"
+#
+        #bgcanvas.create_text(200, 30, text=miniHeader, fill="#C8AA6E", font=("Beaufort for LOL", 14), width=380)
+        #bgcanvas.create_text(200, 70, text=mainHeader, fill="#F0E6D2", font=("Beaufort for LOL", 28), width=380)
+        #bgcanvas.create_text(200, 120, text=infoHeader1, fill="#A09B8C", font=("Spiegel Regular", 12), width=380)
+        #bgcanvas.create_text(200, 140, text=infoHeader2, fill="#A09B8C", font=("Spiegel Regular", 12), width=380)
+        #bgcanvas.pack()
+#
+        ## Region select
+        #regions= ["EUNE", "EUW", "BR", "LAN", "LAS", "NA", "OCE", "RU", "TR", "JP", "KR", "PH", "SG", "TW", "TH", "VN"]
+        #currentVar = ttk.StringVar()
+        #regionSelector = ttk.Combobox(bgcanvas, width= 30, textvariable= currentVar, values=regions)
+        #regionSelector.pack(pady=170, expand="yes")
+#
+        ## Summoner name select
+        #summonerEntry = ttk.Entry(bgcanvas, width=30, textvariable="Your summoner name")
+        #summonerEntry.pack(pady=0, expand="yes")
 
 if __name__ == "__main__":
     credential()
